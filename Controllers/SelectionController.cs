c00e8e99cacefc50891bcfd0dc9a03400fd76fe1
Tom (Thanh Le) <thanh.le@fishwell.no>
2024-05-06 09:05:41 +0700

    Create api selection categories

---
+++ Controllers/SelectionController.cs
@@ -0,0 +1,34 @@
+﻿using ProductManagementBE.Models.Selections.Response;
+
+namespace ProductManagementBE.Controllers
+{
+    [Route("/selection")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [Authorize()]
+    public class SelectionController : ControllerBase
+    {
+        private readonly ProductManagementDbContext _productManagementDbContext;
+
+        public SelectionController(ProductManagementDbContext productManagementDbContext)
+        {
+            _productManagementDbContext = productManagementDbContext;
+        }
+
+        [HttpGet("categories")]
+        public async Task<ActionResult> GetCategorySelectionAsync()
+        {
+            var categories = await _productManagementDbContext.Categories
+                .AsNoTracking()
+                .Where(_ => !_.IsDeleted)
+                .Select(_ => new BaseSelectionResponse
+                {
+                    Id = _.Id,
+                    Name = _.Name,
+                })
+                .ToListAsync();
+
+            return Ok(categories);
+        }
+    }
+}
---
+++ Models/Selections/Response/BaseSelectionResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductManagementBE.Models.Selections.Response
+{
+    public class BaseSelectionResponse
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+    }
+}
